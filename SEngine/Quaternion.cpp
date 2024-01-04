cea51eb82238ec9d004cb53f891dfd80f8abf0b6
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2024-01-04 11:40:43 +0900

    ✨ Quaternion 아직 구현은 안 넣었다

--- SEngine/Quaternion.cpp
+++ SEngine/Quaternion.cpp
@@ -1,3 +1,99 @@
+#include <cmath>
 #include "Quaternion.h"
 
+
 const Quaternion Quaternion::Identity(0.f, 0.f, 0.f, 1.f);
+
+/// <summary>
+/// 두 회전 a와 b 사이의 각도를 도 단위로 반환합니다.
+/// </summary>
+/// <param name="inQuaternion1"></param>
+/// <param name="inQuaternion2"></param>
+/// <returns></returns>
+float Quaternion::Angle(const Quaternion& inQuaternion1, const Quaternion& inQuaternion2)
+{
+	return 0.0f;
+}
+
+/// <summary>
+/// z축을 중심으로 z도, x축을 중심으로 x도, y축을 중심으로 y도 회전하는 회전을 반환하며, 이 순서대로 적용됩니다.
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <param name="z"></param>
+/// <returns></returns>
+Quaternion Quaternion::Euler(float x, float y, float z)
+{
+	return Quaternion();
+}
+
+/// <summary>
+/// fromDirectionVector에서 toDirectionVector로 회전하는 회전을 생성합니다.
+/// </summary>
+/// <param name="fromDirectionVector"></param>
+/// <param name="toDirectionVector"></param>
+/// <returns></returns>
+Quaternion Quaternion::FromToRotation(const Vector3& fromDirectionVector, const Vector3& toDirectionVector)
+{
+	return Quaternion();
+}
+
+/// <summary>
+/// 지정된 앞쪽 및 위쪽 방향으로 회전을 생성합니다
+/// </summary>
+/// <param name="forwardVector"></param>
+/// <param name="upVector"></param>
+/// <returns></returns>
+Quaternion Quaternion::LookRotation(const Vector3& forwardVector, const Vector3& upVector /*= Vector3::Up*/)
+{
+	return Quaternion();
+}
+
+/// <summary>
+/// 매개변수 alpha는 [0, 1] 범위로 고정되며, 쿼터니언 a와 b 사이를 비율 inRatio로 구형으로 보간합니다.
+/// </summary>
+/// <param name="startQuaternion"></param>
+/// <param name="endQuaternion"></param>
+/// <param name="alpha"></param>
+/// <returns></returns>
+Quaternion Quaternion::Slerp(const Quaternion& startQuaternion, const Quaternion& endQuaternion, float inRatio)
+{
+	Quaternion q1 = startQuaternion, q2 = endQuaternion;
+
+	// 사원수의 내적 구하기
+	float dot = q1.x * q2.x + q1.y * q2.y + q1.z * q2.z + q1.w * q2.w;
+
+	// 내적 값이 0보다 작으면 최단거리를 사용하도록 방향을 전환
+	if (dot < 0.0f) {
+		q1 = -q1;
+		dot = -dot;
+	}
+
+	float alpha = 1.f, beta = 0.f;
+	// 두 사원수의 사잇각이 작으면 선형 보간으로 수행
+	if (dot > 0.9995f)
+	{
+		alpha = 1.0f - inRatio;
+		beta = inRatio;
+	}
+	else
+	{
+		const float theta = acosf(dot);
+		const float invSin = 1.f / sinf(theta);
+		alpha = sinf((1.f - inRatio) * theta) * invSin;
+		beta = sinf(inRatio * theta) * invSin;
+	}
+
+	Quaternion result;
+	result.x = alpha * q1.x + beta * q2.x;
+	result.y = alpha * q1.y + beta * q2.y;
+	result.z = alpha * q1.z + beta * q2.z;
+	result.w = alpha * q1.w + beta * q2.w;
+
+	return result;
+}
+
+Quaternion Quaternion::operator-() const
+{
+	return Quaternion(-x, -y, -z, -w);
+}
--- SEngine/Quaternion.h
+++ SEngine/Quaternion.h
@@ -6,7 +6,8 @@
 /// Quaternion i = j = k = sqrt(-1) 3가지 축에 대한 회전을 표현
 /// float X Y Z W
 /// 
-/// TODO: 써보고 다른 곳에서 필요한 내용 추가하기
+/// TODO: Angle / Euler / FromToRotation / LookRotation 구현 필요
+/// 
 /// https://docs.unity3d.com/ScriptReference/Quaternion.html
 /// 쿼터니언은 회전을 표현하는 데 사용됩니다.
 /// 크기가 작고 짐벌 락의 영향을 받지 않으며 쉽게 보간할 수 있습니다. 
@@ -37,7 +38,13 @@ struct Quaternion
 	// Public Methods
 
 	// Static Methods
+	static float Angle(const Quaternion& inQuaternion1, const Quaternion& inQuaternion2);
+	static Quaternion Euler(float x, float y, float z);
+	static Quaternion FromToRotation(const Vector3& fromDirectionVector, const Vector3& toDirectionVector);
+	static Quaternion LookRotation(const Vector3& forwardVector, const Vector3& upVector = Vector3::Up);
+	static Quaternion Slerp(const Quaternion& startQuaternion, const Quaternion& endQuaternion, float alpha);
 
-
+	// Operators
+	Quaternion operator-() const;
 };
 
