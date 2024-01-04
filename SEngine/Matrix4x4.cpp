0c520c29b5ca5781ba69f7f187c5844cbb81dec4
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2024-01-04 10:46:06 +0900

    ✨ Matrix4x4 써보고 고치기

--- SEngine/Matrix4x4.cpp
+++ SEngine/Matrix4x4.cpp
@@ -1,1 +1,65 @@
+#include <cassert>
+
 #include "Matrix4x4.h"
+
+
+const Matrix4x4 Matrix4x4::Identity(
+	Vector4(1.0f, 0.0f, 0.0f, 0.0f),
+	Vector4(0.0f, 1.0f, 0.0f, 0.0f),
+	Vector4(0.0f, 0.0f, 1.0f, 0.0f),
+	Vector4(0.0f, 0.0f, 0.0f, 1.0f)
+);
+
+Matrix4x4 Matrix4x4::Transpose() const
+{
+	return Matrix4x4(
+		Vector4(cols[0].x, cols[1].x, cols[2].x, cols[3].x),
+		Vector4(cols[0].y, cols[1].y, cols[2].y, cols[3].y),
+		Vector4(cols[0].z, cols[1].z, cols[2].z, cols[3].z),
+		Vector4(cols[0].w, cols[1].w, cols[2].w, cols[3].w)
+	);
+}
+
+const Vector4& Matrix4x4::operator[](unsigned int inIndex) const
+{
+	assert(inIndex < colCount);
+	return cols[inIndex];
+}
+
+Vector4& Matrix4x4::operator[](unsigned int inIndex)
+{
+	assert(inIndex < colCount);
+	return cols[inIndex];
+}
+
+Matrix4x4 Matrix4x4::operator*(const Matrix4x4& inMatrix) const
+{
+	Matrix4x4 TM = Transpose();
+	return Matrix4x4(
+		Vector4(TM[0].Dot(inMatrix[0]), TM[1].Dot(inMatrix[0]), TM[2].Dot(inMatrix[0]), TM[3].Dot(inMatrix[0])),
+		Vector4(TM[0].Dot(inMatrix[1]), TM[1].Dot(inMatrix[1]), TM[2].Dot(inMatrix[1]), TM[3].Dot(inMatrix[1])),
+		Vector4(TM[0].Dot(inMatrix[2]), TM[1].Dot(inMatrix[2]), TM[2].Dot(inMatrix[2]), TM[3].Dot(inMatrix[2])),
+		Vector4(TM[0].Dot(inMatrix[3]), TM[1].Dot(inMatrix[3]), TM[2].Dot(inMatrix[3]), TM[3].Dot(inMatrix[3]))
+	);
+}
+
+Matrix4x4 Matrix4x4::operator*(float inScalar) const
+{
+	return Matrix4x4(
+		cols[0] * inScalar,
+		cols[1] * inScalar,
+		cols[2] * inScalar,
+		cols[3] * inScalar
+	);
+}
+
+Vector4 Matrix4x4::operator*(const Vector4& inVector) const
+{
+	Matrix4x4 TM = Transpose();
+	return Vector4(
+		TM[0].Dot(inVector),
+		TM[1].Dot(inVector),
+		TM[2].Dot(inVector),
+		TM[3].Dot(inVector)
+	);
+}
--- SEngine/Matrix4x4.h
+++ SEngine/Matrix4x4.h
@@ -8,6 +8,8 @@
 /// Transform <-> Matrix4x4 변환
 /// float 16개 4줄로 4개씩 
 /// 
+/// TODO: Vector4개 -> float 16개로 바꾸기
+/// 
 /// https://docs.unity3d.com/ScriptReference/Matrix4x4.html
 /// transformation matrix는 임의의 3D transformations(예: 이동, 회전, 스케일, 전단 등)과 homogenous coordinates를 사용한 perspective transformations을 수행할 수 있습니다. 
 /// 스크립트에서 행렬을 사용하는 경우는 거의 없으며, 대부분 Vector3, 쿼터니언 및 Transform 클래스의 기능을 사용하는 것이 더 간단합니다.
@@ -17,12 +19,28 @@
 /// </summary>
 struct Matrix4x4
 {
+	// Constructors
+	Matrix4x4() = default;
+	Matrix4x4(const Vector4& inCol0, const Vector4& inCol1, const Vector4& inCol2, const Vector4& inCol3) { cols = { inCol0, inCol1, inCol2, inCol3 }; }
+
+	//Public Methods
+	Matrix4x4 Transpose() const;
+
+	// Operators
+	const Vector4& operator[](unsigned int inIndex) const;
+	Vector4& operator[](unsigned int inIndex);
+
+	Matrix4x4 operator*(const Matrix4x4& inMatrix) const;
+	Matrix4x4 operator*(float inScalar) const;
+
+	Vector4 operator*(const Vector4& inVector) const;
+
 	// Static Properties
 	static const Matrix4x4 Identity;
 	static const Matrix4x4 Zero;
 	static constexpr unsigned int colCount = 4;
 
 	// Properties
-	std::array<Vector4, colCount> columns = { Vector4::UnitX, Vector4::UnitY, Vector4::UnitZ, Vector4::UnitW };
+	std::array<Vector4, colCount> cols = { Vector4::UnitX, Vector4::UnitY, Vector4::UnitZ, Vector4::UnitW };
 };
 
