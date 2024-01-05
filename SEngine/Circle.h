9401fff8f481e2a8797cf1d942b1900d45ecb372
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2024-01-05 17:37:00 +0900

    🚧 Sphere Box 먼저 하기

--- SEngine/Circle.h
+++ SEngine/Circle.h
@@ -1,6 +1,18 @@
 #pragma once
 
+/// <summary>
+/// 원 표현, 원-원 충돌
+/// center 좌표 / 반지름
+/// 
+/// 3D여서 아마도 디버깅용으로만 쓸 것 같다 나중에 만들기 
+/// 
+/// 240105 이서영
+/// </summary>
 struct Circle
 {
+	Circle() = default;
+
+
+
 };
 
